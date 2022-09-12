124b06b901fbe72e373206d2af98371976efa0d6
Suyanshu9 <113342323+Suyanshu9@users.noreply.github.com>
2022-09-12 15:32:56 +0530

    learn

--- learn.cpp
+++ learn.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main({
+    int n;
+    cout << "Enter the number"<< endl;
+    cin>> n;
+
+    for(int i= 0;i<= 100;i++){
+        for(int j = 0;j<= i; j++){
+            cout << i;
+        }
+        cout << endl;
+        if(i>n){
+            break;
+        }
+    }
+
+})
